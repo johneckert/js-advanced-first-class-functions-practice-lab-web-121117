840ba4171b710e830d352a5f220a1c03258858d6
johneckert <johnteckert@gmail.com>
2018-01-31 19:36:47 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(drivers, town) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown == town) {
+      console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(drivers) {
+  const relevantDrivers = [...drivers];
+  return relevantDrivers.sort(function(a, b) {
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(drivers) {
+  const sortedDrivers = [...drivers];
+  return sortedDrivers.sort(function(a, b) {
+    return a.name > b.name;
+  }, 0);
+}
+
+function totalRevenue(drivers) {
+  const revenue = drivers.map(driver => driver.revenue);
+  return revenue.reduce(function(a, b) {
+    return a + b;
+  });
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
